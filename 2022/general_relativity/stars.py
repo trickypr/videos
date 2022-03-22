eb21392fd774ece69d4fc6346a815eb5a8205475
trickypr <trickypr@icloud.com>
2022-03-23 09:16:49 +1100

    ✨ Add orbiting planets

--- 2022/general_relativity/stars.py
+++ 2022/general_relativity/stars.py
@@ -1,5 +1,7 @@
 from random import random
 from manim import *
+from numpy import linalg as la
+import operator as op
 
 
 class StarBackground(Scene):
@@ -17,3 +19,69 @@ class StarBackground(Scene):
 
         self.play(*[Write(point, run_time=random() * 2) for point in points])
         self.wait()
+
+
+class Planet(VGroup):
+    proportion: float = 0.0
+    rate: float = 100
+
+    def __init__(self, star, *mobjects, **kwargs):
+        super().__init__(*mobjects, **kwargs)
+
+        self.orbit = Circle(radius=2.0).set_fill(None,
+                                                 0).set_stroke(WHITE, 0.2)
+        self.planet = Circle(radius=0.2, color=GREEN).move_to(
+            ORIGIN)  # TODO: Correct color for mercury
+        self.planet.move_to(self.orbit.point_from_proportion(0))
+        self.star = star
+
+    def get_draw_in(self):
+        return [Write(self.orbit), Write(self.planet)]
+
+    def start_animation(self):
+        self.add_updater(lambda _m, dt: self.on_update(dt))
+
+    def on_update(self, dt: float = 0, recursive: bool = True):
+        print(self.proportion)
+
+        orbit = self.orbit
+        planet = self.planet
+        star = self.star
+
+        rate = self.rate
+        radius_vector = planet.get_center() - star.get_center()
+        rate *= 1.0 / la.norm(radius_vector)
+
+        prop = self.proportion
+        d_prop = 0.001
+        ds = la.norm(
+            op.add(
+                orbit.point_from_proportion((prop + d_prop) % 1),
+                -orbit.point_from_proportion(prop),
+            ))
+
+        delta_prop = (d_prop / ds) * rate * dt
+
+        self.proportion = (self.proportion + delta_prop) % 1
+        planet.move_to(orbit.point_from_proportion(self.proportion))
+
+
+class OrbitingPlanets(Scene):
+    def construct(self):
+        text = Text('(Fairly standard orbit)',
+                    font_size=25).to_corner(UP + LEFT)
+        sun = Circle(radius=0.5, color=YELLOW).move_to(ORIGIN)
+        orbit = Circle(radius=3.0).set_fill(None, 0).set_stroke(WHITE, 0.2)
+        planet = Dot(RIGHT * 3)
+
+        self.play(Write(sun), Write(planet), Write(orbit), Write(text))
+
+        for _ in range(0, 3):
+            self.play(
+                Rotating(planet,
+                         about_point=ORIGIN,
+                         angle=TAU,
+                         run_time=TAU,
+                         rate_func=linear))
+
+        self.play(Unwrite(sun), Unwrite(planet), Unwrite(orbit), Unwrite(text))
